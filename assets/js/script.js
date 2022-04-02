78c08329291425f98aede1bf57b7134dedeba056
A-Wonderfull-Galaxy <marlonq16@gmail.com>
2022-04-01 20:39:46 -0400

    add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,4 @@
 var taskIdCounter = 0;
-<<<<<<< HEAD
-var formEl = document.querySelector("#task-form"); 
-var tasksToDoEl = document.querySelector("#tasks-to-do"); 
-
-var taskFormHandler = function(event) {
-    event.preventDefault();
-    var taskNameInput = document.querySelector("input[name='task-name']").value;
-    var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  
-    // package up data as an object
-    var taskDataObj = {
-      name: taskNameInput,
-      type: taskTypeInput
-    };
-  
-    //check if input values are empty strings
-    if (!taskNameInput || !taskTypeInput){
-        alert("You need to fill out task form!")
-        return false;
-    }
-
-    formEl.reset();
-    
-    // send it as an argument to createTaskEl
-    createTaskEl(taskDataObj);
-}
-
-var createTaskEl = function(taskDataObj) {
-    //create list item
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-  
-    //listItemEl.setAttribute("data-task-id", taskIdCounter);
-  
-    // create div to hold task info and add to list item
-    var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-    listItemEl.appendChild(taskInfoEl);
-
-    //var taskActionsEl = createTaskActions(taskIdCounter);
-    //listItemEl.appendChild(taskActionsEl);
-  
-    //add entire list item to list
-    tasksToDoEl.appendChild(listItemEl);
-  
-    // increase task counter for next unique id
-    //taskIdCounter++;
-};
-
-formEl.addEventListener("submit", taskFormHandler);
-=======
 
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
@@ -254,4 +202,3 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 
 // for changing the status
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
->>>>>>> feature/updating-tasks
